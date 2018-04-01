3538b8e13aafa6246a5981a631e7b1ad2a70d6c9
Lukas Cedronas <lukascedronas1@gmail.com>
2018-04-02 00:52:07 +0300

    Initial commit

---
+++ src/main/java/com/example/tech_plat_1uzd/TechPlat1uzdApplication.java
@@ -0,0 +1,12 @@
+package com.example.tech_plat_1uzd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPlat1uzdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechPlat1uzdApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/tech_plat_1uzd/TechPlat1uzdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.tech_plat_1uzd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechPlat1uzdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
